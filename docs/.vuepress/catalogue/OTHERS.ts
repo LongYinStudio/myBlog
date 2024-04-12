cb17f15473fbe43197ef3f9931bc2012fb9851d9
LongYinStudio <1606776851@qq.com>
2024-04-12 20:43:43 +0800

    feat(新增文章):虚拟机压缩磁盘空间

---
+++ docs/.vuepress/catalogue/OTHERS.ts
@@ -0,0 +1,6 @@
+export default [
+  {
+    text: "虚拟机压缩磁盘空间",
+    link: "/OTHERS/virtual-disk.md",
+  },
+];
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@ import IDE from "./catalogue/IDE";
 import JS from "./catalogue/JS";
 import VUE from "./catalogue/VUE";
 import CSS from "./catalogue/CSS";
+import OTHERS from "./catalogue/OTHERS";
 
 export default defineUserConfig({
   lang: "zh-CN",
@@ -36,6 +37,10 @@ export default defineUserConfig({
       {
         text: "IDE",
         children: IDE,
+      },
+      {
+        text: "其他",
+        children: OTHERS,
       },
       {
         text: "龙吟工作室",
